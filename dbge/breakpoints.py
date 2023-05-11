d4bfc32f6f82a83ad76f8668977bf1e3234dd17b
aranega <vincent.aranega@gmail.com>
2023-05-10 22:46:04 -0600

    Fix issue with external/internal mode

--- dbge/breakpoints.py
+++ dbge/breakpoints.py
@@ -57,17 +57,31 @@ class AttributeBasedBreakpoint(BytecodeBasedBreakpoint):
             return False
 
         # Check then the attribute name
-        codeobj = frame.f_code
         if bc.argval != self.attribute:
             return False
 
         if self.mode != 'both':
             # If internal or external access only must be considered
             # We get the current object
-            self_name = codeobj.co_varnames[0] if codeobj.co_argcount > 0 else ""
-            context_of = frame.f_locals.get(self_name, None)
-            context_ok = ((self.mode in ('internal', 'i') and context_of is self.instance) or
-                          (self.mode in ('external', 'e') and context_of is not self.instance))
+            # self_name = codeobj.co_varnames[0] if codeobj.co_argcount > 0 else ""
+            # context_of = frame.f_locals.get(self_name, None)
+            # context_ok = ((self.mode in ('internal', 'i') and context_of is self.instance) or
+            #               (self.mode in ('external', 'e') and context_of is not self.instance))
+
+            codeobj = frame.f_code
+
+            # We build the potential full qual name of the current function/method
+            type_name = self.instance.__class__.__qualname__
+            fun_name = codeobj.co_name
+            build_qualname = f"{type_name}.{fun_name}"
+
+            # We take the qual name of the codeobj
+            qualname = codeobj.co_qualname
+
+            # We check if they match
+            is_inside = qualname == build_qualname or qualname.endswith(f".{build_qualname}")
+            context_ok = ((self.mode in ('internal', 'i') and is_inside) or
+                          (self.mode in ('external', 'e') and not is_inside))
         else:
             # If the access must be considered in functions that are part of the object
             context_ok = True
